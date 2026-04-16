17b1ff18abefa92688cec291bd5986e8d563b3e4
Deva658 <devaaditya271@gmail.com>
2026-04-16 07:14:43 +0700

    deklarasi namespace UCP dan referensi library SQL Client untuk FormPetani

--- UCP/FormPetani.cs
+++ UCP/FormPetani.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace UCP
+{
+    public partial class FormPetani : Form
+    {
+        
+    }
+}
