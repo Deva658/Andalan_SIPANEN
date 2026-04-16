8b057107986554cff92f5bfb969c337c7e60ae50
Deva658 <devaaditya271@gmail.com>
2026-04-16 07:03:48 +0700

    inisialisasi constructor FormAdmin

--- UCP/FormAdmin.cs
+++ UCP/FormAdmin.cs
@@ -9,6 +9,18 @@ namespace UCP
         private readonly SqlConnection conn;
         private readonly string connectionString = "Data Source=DEVA\\DEPA15;Initial Catalog=DB_HasilPanen;Integrated Security=True";
 
+        public FormAdmin()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+
+            this.Load += FormAdmin_Load;
+            btnLoad.Click += btnLoad_Click;
+            btnDelete.Click += btnDelete_Click;
+            btnLogout.Click += btnLogout_Click;
+            txtCari.TextChanged += txtCari_TextChanged;
+        }
+
         
     }
 }
