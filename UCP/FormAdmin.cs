41642618dc07a69d633099e402ddf0a9c56a9804
Deva658 <devaaditya271@gmail.com>
2026-04-16 07:02:44 +0700

    inisialisasi kelas dan koneksi database

--- UCP/FormAdmin.cs
+++ UCP/FormAdmin.cs
@@ -4,5 +4,11 @@ using System.Windows.Forms;
 
 namespace UCP
 {
-    
+    public partial class FormAdmin : Form
+    {
+        private readonly SqlConnection conn;
+        private readonly string connectionString = "Data Source=DEVA\\DEPA15;Initial Catalog=DB_HasilPanen;Integrated Security=True";
+
+        
+    }
 }
