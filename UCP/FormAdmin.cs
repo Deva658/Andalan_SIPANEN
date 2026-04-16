1d4ef9dd00339775a45d0b132a61b991dd660709
Deva658 <devaaditya271@gmail.com>
2026-04-16 07:06:03 +0700

    menambahkan fungsi pada tombol load

--- UCP/FormAdmin.cs
+++ UCP/FormAdmin.cs
@@ -40,6 +40,41 @@ namespace UCP
             dataGridView1.Columns.Add("Kualitas", "Kualitas");
         }
 
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+
+                dataGridView1.Rows.Clear();
+
+                string queryAdmin = @"SELECT h.id_panen, p.nama_petani, p.no_telp, t.nama_tanaman, h.tanggal_panen, h.jumlah_hasil, t.satuan_hasil, h.kualitas 
+                              FROM Hasil_Panen h
+                              JOIN Petani p ON h.id_petani = p.id_petani
+                              JOIN Tanaman t ON h.id_tanaman = t.id_tanaman";
+
+                SqlCommand cmd = new SqlCommand(queryAdmin, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(
+                        reader["id_panen"].ToString(),
+                        reader["nama_petani"].ToString(),
+                        reader["no_telp"].ToString(),
+                        reader["nama_tanaman"].ToString(),
+                        Convert.ToDateTime(reader["tanggal_panen"]).ToShortDateString(),
+                        reader["jumlah_hasil"].ToString(),
+                        reader["satuan_hasil"].ToString(),
+                        reader["kualitas"].ToString()
+                    );
+                }
+                reader.Close();
+                conn.Close();
+            }
+           
+        }
+
         
     }
 }
