86c7c12e29ab3d642cdb0d0b428d404cb16f7ed5
Deva658 <devaaditya271@gmail.com>
2026-05-14 02:09:35 +0700

    Setup parameter login dan wiring event handler tombol utama FormPetani

--- UCP/FormPetani.cs
+++ UCP/FormPetani.cs
@@ -13,3 +13,19 @@ namespace UCP
         private readonly string connectionString = "Data Source=DEVA\\DEPA15;Initial Catalog=DB_HasilPanen;Integrated Security=True";
 
         private string idPetaniLogin;
+
+        public FormPetani(string idPetani, string namaPetani)
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+
+            idPetaniLogin = idPetani;
+
+            txtNamaPetani.Text = namaPetani;
+
+            this.Load += FormPetani_Load;
+            btnLoad.Click += btnLoad_Click;
+            btnInsert.Click += btnInsert_Click;
+            btnUpdate.Click += btnUpdate_Click;
+            btnLogout.Click += btnLogout_Click;
+        }
