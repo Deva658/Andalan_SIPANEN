f148346801040666895194544b6ad40e0ae1de2d
Deva658 <devaaditya271@gmail.com>
2026-05-14 02:01:01 +0700

    Inisialisasi variabel BindingSource dan DataTable

--- UCP/FormPetani.cs
+++ UCP/FormPetani.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace UCP
+{
+    public partial class FormPetani : Form
+    {
+        private BindingSource bindingSource1 = new BindingSource();
+        private DataTable dtPanen = new DataTable();
+        private readonly SqlConnection conn;
+        private readonly string connectionString = "Data Source=DEVA\\DEPA15;Initial Catalog=DB_HasilPanen;Integrated Security=True";
+
+        private string idPetaniLogin;
